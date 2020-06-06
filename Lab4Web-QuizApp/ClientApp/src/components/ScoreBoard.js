a7d58f36cffc2d604de4d83ad696d0b8e1bab06f
Luna <Hanken95@gmail.com>
2020-06-06 19:55:38 +0200

    responsive scoreboard

--- Lab4Web-QuizApp/ClientApp/src/components/ScoreBoard.js
+++ Lab4Web-QuizApp/ClientApp/src/components/ScoreBoard.js
@@ -50,28 +50,24 @@ class ScoreBoard extends Component {
 
     renderScores() {
         return (
-            <div className="border p-3 mt-3">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Score Id:</th>
-                            <th>Submitted:</th>
-                            <th>Score:</th>
-                            <th>Username:</th>
-                        </tr>
-                        {
-                            this.state.highScores.map(entrance => (
-                                <tr key={entrance.id}>
-                                    <td>{entrance.id}</td>
-                                    <td>{entrance.dateSubmitted}</td>
-                                    <td>{entrance.score}</td>
-                                    <td>{entrance.username}</td>
-                                </tr>
-                            ))
-                        }
-                    </thead>
-                </table>
-            </div>
+            <table className="table table-striped" class="score-table">
+                <thead>
+                    <tr>
+                        <th>Submitted:</th>
+                        <th>Score:</th>
+                        <th>Username:</th>
+                    </tr>
+                    {
+                        this.state.highScores.map(entrance => (
+                            <tr key={entrance.id}>
+                                <td>{entrance.dateSubmitted}</td>
+                                <td>{entrance.score}</td>
+                                <td>{entrance.username}</td>
+                            </tr>
+                        ))
+                    }
+                </thead>
+            </table>
         )
     }
 
